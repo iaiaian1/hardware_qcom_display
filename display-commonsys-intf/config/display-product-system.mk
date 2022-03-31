c30e292beb364150bced466012a3fcc0bd61b377
Jake <yapjakebrian1@gmail.com>
2022-03-31 19:40:11 +0800

    Import all 5 versions of ndk_platform
    Signed-off-by: Jake <yapjakebrian1@gmail.com>

--- display-commonsys-intf/config/display-product-system.mk
+++ display-commonsys-intf/config/display-product-system.mk
@@ -13,7 +13,12 @@ PRODUCT_PACKAGES += libdisplayconfig \
                     liblayerext.qti \
                     libsmomoconfig.qti \
                     libcomposerextn.qti \
-                    libdisplayconfig.qti
+                    libdisplayconfig.qti \
+                    vendor.qti.hardware.display.config-V1-ndk_platform \
+                    vendor.qti.hardware.display.config-V2-ndk_platform \
+                    vendor.qti.hardware.display.config-V3-ndk_platform \
+                    vendor.qti.hardware.display.config-V4-ndk_platform \
+                    vendor.qti.hardware.display.config-V5-ndk_platform
 
 SOONG_CONFIG_NAMESPACES += qtidisplaycommonsys
 # Soong Keys
